2287b385fcf4fb02da8c7623df158bcf13880721
Sourabhhhhh <sourabhrush001@gmail.com>
2024-08-03 00:03:12 +0530

    Create Question.java
    CLASS - Question

---
+++ Question.java
@@ -0,0 +1,27 @@
+package quiz;
+
+import java.util.List;
+
+public class Question {
+    private final String question;
+    private final List<String> options;
+    private final int correctOptionIndex;
+
+    public Question(String question, List<String> options, int correctOptionIndex) {
+        this.question = question;
+        this.options = options;
+        this.correctOptionIndex = correctOptionIndex;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public int getCorrectOptionIndex() {
+        return correctOptionIndex;
+    }
+}
